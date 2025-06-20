f17ae60145e8729f4370afdbea9028aad8350d3f
dam80-me <malinda_madurawela@yahoo.com>
2025-06-20 00:03:00 -0700

    fourth commit

---
+++ 15th.py
@@ -0,0 +1,26 @@
+try:
+    user_input = input("Enter a list of numbers seperated by commas e.g.,")
+    numbers_str= user_input.split(',')
+    numbers = []
+
+    if not numbers_str or (len(numbers_str)==1 and not numbers_str[0]):
+        raise IndexError("The list is empty.")
+    
+    for num_str in numbers_str:#234
+        numbers.append(int(num_str.strip()))
+
+
+except ValueError:
+    print("Error: Non-numeric input encountered. Please enter only")
+except IndexError as e:
+    print(f"Error: {e}")
+
+
+
+else:
+    total_sum = sum (numbers)
+    print (f"The sum of the numbers is: {total_sum}")
+
+
+finally:
+    print("Operation completed.")
